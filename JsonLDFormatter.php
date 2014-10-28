efe61b5ee5af96c05684696536e4538c7c8a8d5f
Jian Zhao <jzhao@astreetpress.com>
2014-10-28 14:40:55 +0800

    add json-ld formatter

--- JsonLDFormatter.php
+++ JsonLDFormatter.php
@@ -12,20 +12,67 @@ class JsonLDFormatter
 {
 
     private $thing;
+    private $json;
 
     public function __construct(Thing $thing)
     {
         $this->thing = $thing;
+        $this->json = new stdClass();
     }
 
-    public function format()
+    public function format($topLevel = true)
     {
         $reflectionClass = new ReflectionClass($this->thing);
         $properties = $reflectionClass->getProperties();
-        var_dump($properties);
+        foreach ($properties as $property) {
+            $getter = 'get' . ucfirst($property->name);
+            if (!is_null($this->thing->$getter())) {
+
+                if ($property->name == 'context') {
+
+                    $parts = explode('/', $this->thing->$getter());
+                    $type = array_pop($parts);
+
+                    if ($topLevel) {
+                        $context = implode('/', $parts);
+                        $label = '@context';
+                        $this->json->{$label} = $context;
+                    }
+
+                    $label = '@type';
+                    $this->json->{$label} = $type;
+                } else {
+                    // Text based property.
+                    $doc = $property->getDocComment();
+                    $match = array();
+                    preg_match('/@var ([a-zA-Z\\\\]+)/', $doc, $match);
+
+                    switch ($match[1])
+                    {
+                        case 'String':
+                            $this->json->{$property->name} = $this->thing->$getter();
+                            break;
+                        case 'Integer':
+                        case 'float':
+                            throw new Exception('no implemented');
+                            break;
+                        default:
+                            $newJsonLDFormatter = new JsonLDFormatter($this->thing->$getter());
+                            $this->json->{$property->name} = $newJsonLDFormatter->format(false);
+                            break;
+                    }
+                }
+            }
+        }
+
+        return $this->json;
+    }
+
+    public function toJson()
+    {
+        echo json_encode($this->json);
     }
 }
-
 require 'vendor/autoload.php';
 
 $loader = new Composer\Autoload\ClassLoader();
@@ -38,11 +85,13 @@ $videoObject = new VideoObject();
 $person = new Thing\Person();
 $person->setTelephone('123456789');
 $person->setName('Robin');
-
 $videoObject->setAuthor($person);
+
+$videoObject->setAward("This is a award.");
+
+
 
 $formatter = new JsonLDFormatter($videoObject);
 
-$json = $formatter->format();
-
-echo $json;
+$formatter->format();
+$formatter->toJson();
