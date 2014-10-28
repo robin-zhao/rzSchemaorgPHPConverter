781e23aa8e5e44e744627a206c7874f0d35fffe0
Jian Zhao <jzhao@astreetpress.com>
2014-10-28 10:49:35 +0800

    add file doc

--- Example/Thing/Intangible/Audience/EducationalAudience.php
+++ Example/Thing/Intangible/Audience/EducationalAudience.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * This is an auto generated file.
+ * You are encouraged to edit the script below:
+ * https://github.com/robin-zhao/rzSchemaorgPHPConverter/
+ */
 
 namespace Example\Thing\Intangible\Audience;
 
